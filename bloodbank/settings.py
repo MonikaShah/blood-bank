f58691585941c5b7ae53c054e2e48055e9ba91a9
monikashah <monikapatira@gmail.com>
2024-10-30 15:55:45 +0530

    settings

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -134,8 +134,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # or another appropriate directory
+#STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
